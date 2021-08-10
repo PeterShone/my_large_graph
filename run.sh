49568ac1526a186c7c055eb846d7d1e182b56802
petershone <515663728@qq.com>
2021-08-10 11:43:14 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --partition=gpu_24h
+#SBATCH --gres=gpu:1
+#SBATCH --output=out.log
+eval "$(/research/dept8/fyp21/cwf2101/peter/miniconda3/bin/conda shell.bash hook)"
+conda activate graph
+CUDA_LAUNCH_BLOCKING=1 python3 scripts/train_network.py
+

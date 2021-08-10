81c03793fa5124c63deb6f56972a1f1741c3209b
petershone <515663728@qq.com>
2021-08-10 11:39:02 +0800

    coll-1, area-1, sample-1

--- scripts/train_network.py
+++ scripts/train_network.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     parser.add_argument("-c",
                         "--config",
                         type=str,
-                        default=parentdir + "/configs/config.json")
+                        default=parentdir + "/configs/extra_1.json")
     args = parser.parse_args()
 
     with open(args.config, "r") as f:
